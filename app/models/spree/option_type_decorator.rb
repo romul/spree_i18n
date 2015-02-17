88daa70209e215d0d187cd29e651bae843edcbb5
Roman Smirnov <POMAHC@gmail.com>
2015-02-17 12:19:58 +0300

    Fixed db:schema:load on empty db. #512

--- app/models/spree/option_type_decorator.rb
+++ app/models/spree/option_type_decorator.rb
@@ -2,5 +2,5 @@ module Spree
   OptionType.class_eval do
     translates :name, :presentation, fallbacks_for_empty_translations: true
     include SpreeI18n::Translatable
-  end
+  end if OptionType.table_exists?
 end
